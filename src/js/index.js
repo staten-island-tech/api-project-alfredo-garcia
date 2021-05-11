cc9fb4813da559df029f9a9f873e95c776087beb
Alfredo5421 <alfredo.garcia23@sitechhs.com>
2021-05-11 17:37:58 -0400

    hard

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,22 @@
-//const key = "YOURKEYHERE";
+//gonna use for loop for now, looking for a substitution, found some but they look complicated
 const FetchPokemon = () => {
-  const url = "https://pokeapi.co/api/v2/pokemon/1";
-  fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const pokemon = {};
-      pokemon["name"] = data.name;
-      pokemon["id"] = data.id;
-      pokemon["image"] = data.sprites["front_default"];
-      pokemon["type"] = data.types.map((type) => type.type.name).join(", ");
-      console.log(pokemon);
-    });
+  for (let i = 1; i <= 150; i++) {
+    const url = "https://pokeapi.co/api/v2/pokemon/${i}";
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const pokemon = {
+          name: data.name,
+          id: data.id,
+          Image: data.sprites["front_default"],
+          type: data.types.map((type) => type.type.name).join(", "),
+        };
+        console.log(pokemon);
+      });
+  }
 };
 //stuff above retrieves certain stuff from api data base that I want
 FetchPokemon();
