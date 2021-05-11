e6bab0beac665b924393c05d0ec0405c1e0ec62a
Alfredo5421 <alfredo.garcia23@sitechhs.com>
2021-05-11 16:48:45 -0400

    progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
-
-const key = "YOURKEYHERE";
+//const key = "YOURKEYHERE";
+const FetchPokemon = () => {
+  const url = "https://pokeapi.co/api/v2/pokemon/1";
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const pokemon = {};
+      pokemon["name"] = data.name;
+      pokemon["id"] = data.id;
+      pokemon["image"] = data.sprites["front_default"];
+      pokemon["type"] = data.types.map((type) => type.type.name).join(", ");
+      console.log(pokemon);
+    });
+};
+//stuff above retrieves certain stuff from api data base that I want
+FetchPokemon();
